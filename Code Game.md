5e371923f0c450171751b1a5af9070dc0ef11e45
Sinmysize <jadenshellie01@gmail.com>
2025-01-13 13:59:10 -0800

    yeahhh

---
+++ Code Game.md
@@ -0,0 +1,187 @@
+# Code Game
+
+A very stupid game about guessing a code.
+
+Currently none of the endpoints will be active until Jan 31.
+
+## Table of Contents
+
+- [About Code Game](#about-code-game)
+- [Creating an account](#creating-an-account)
+- [Sending a code](#sending-a-code)
+- [Getting user statistics](#sending-a-code)
+- [Deleting your account](#deleting-your-account)
+- [Getting server information](#getting-server-info)
+- Getting the leaderboard (WIP)
+
+## About Code Game
+
+Code Game is a pretty dumb project I made to introduce myself into the [Rust Programming Language](https://www.rust-lang.org/). In Code Game, you attempt to guess/crack a 4 character code. *This may change in the future if it gets too easy for anyone who might be playing this*. I thought it would be funny to make it more into a game to see who can guess the most codes with or without a program.
+
+The likely hood of me doing major updates to this project is pretty slim since I am working on a much larger scale project, so for the few who may enjoy this project do not expect much from this.
+
+## Creating an Account
+
+- **URL** : `/code-game/user/register`
+
+- **Method** : `POST`
+
+- **Auth** : `None`
+
+- **Headers** : `Content-Type: application/json`
+
+**Body** :
+
+```json
+{
+    username: "String",
+    password: "String"
+}
+```
+
+### Response
+
+- **Success** : `200 OK`
+
+    - Returns 200 when you successfully register an account.
+
+- **Error** : `418 I'm a teapot`
+
+    - Returns 418 if an account with the same username exists. *Do not ask why I chose 418*.
+
+## Sending a Code
+
+- **URL** : `/code-game/code`
+
+- **Method** : `POST`
+
+- **Auth** : `None`
+
+- **Headers** : `Content-Type: application/json`
+
+**Body** :
+
+```json
+{
+    code: "String",
+    username: "String",
+    password: "String" // Ensures the code is sent by YOU
+}
+```
+
+### Response
+
+- **Success** : `200 OK`
+
+    - Returns 200 if you guess the code correctly.
+
+- **Failed Code** : `401 Unauthorized`
+
+    - Returns 401 if you guess the code incorrectly.
+
+## Getting User Statistics
+
+- **URL** : `/code-game/user/{username}`
+
+- **Method** : `GET`
+
+- **Auth** : `None`
+
+- **Headers** : `Content-Type: application/json`
+
+- **Body** : `None`
+
+### Response
+
+- **Success** : `200 OK`
+
+    - Returns 200 and user's statistics if user exists.
+
+        - **username** : Given username.
+        - **cracked** : Number of codes guessed correctly.
+        - **rank** : Global rank of user.
+
+*Example*:
+```json
+{
+    username: "John Doe",
+    cracked: "5",
+    rank: "3"
+}
+```
+
+- **Failure** : `404 Not Found`
+
+    - Returns 404 if the user does not exist.
+
+
+## Deleting Your Account
+
+### **[ DISCLAIMER ]**
+
+**ONCE ACCOUNT IS DELETED IT `CANNOT BE RECOVERED`**
+    
+**ALL PROGRESS WILL BE `PERMANENTLY DELETED`**
+
+#
+
+- **URL** : `/code-game/user/{username}`
+
+- **Method** : `DELETE`
+
+- **Auth** : `None`
+
+- **Headers** : `Content-Type: application/json`
+
+**Body** :
+
+```json
+{
+    username: "String",
+    password: "String" // Ensures the request is sent by YOU
+}
+```
+
+### Response
+
+- **Success** : `200 OK`
+
+    - Returns 200 if your account is successfully deleted.
+
+- **Error** : `403 Forbidden`
+
+    - Returns 403 for a few reasons:
+        1. Account has already been deleted.
+        2. Account does not exist.
+        3. Credentials given are not correct.
+
+## Getting Server Info
+
+- **URL** : `/code-game/statistics`
+
+- **Method** : `GET`
+
+- **Auth** : `None`
+
+- **Headers** : `Content-Type: application/json`
+
+- **Body** : `None`
+
+
+### Response
+
+- **Success** : `200 OK`
+
+    - Will always return 200 and server stats.
+        - **code_length** : Length of code.
+        - **server_uptime** : Time since server ran in seconds.
+
+*Example* 
+```json
+{
+    code_length: "4",
+    server_uptime: "248400"
+}
+```
+
+## Getting the Leaderboard
---
+++ src/main.rs
@@ -0,0 +1,250 @@
+use std::{sync::{Arc, Mutex, RwLock}, time::Instant};
+use actix_web::{delete, get, post, web, App, HttpResponse, HttpServer, Responder};
+use dotenvy::dotenv;
+use serde::{Deserialize, Serialize};
+use rand::distributions::{Alphanumeric, DistString};
+use sqlx::{prelude::FromRow, sqlite::SqlitePoolOptions, Pool, Sqlite};
+
+#[derive(Deserialize)]
+struct CredentialBody {
+    username: String,
+    password: String
+}
+
+#[derive(Serialize, FromRow)]
+struct User {
+    username: String,
+    cracked: u16,
+    rank: Option<u32>
+}
+
+#[derive(Serialize)]
+struct WebResponse {
+    message: String,
+}
+
+#[derive(Deserialize)]
+struct WebRequest {
+    code: String,
+    username: String,
+    password: String
+}
+
+#[derive(Serialize)]
+struct Webhook {
+    username: String,
+    avatar_url: Option<String>,
+    content: String
+}
+
+struct AppState {
+    currentcode: RwLock<String>,
+    code_length: RwLock<u8>,
+    client: Arc<Mutex<Pool<Sqlite>>>
+}
+
+#[derive(Serialize)]
+struct Statistics {
+    code_length: u8,
+    server_uptime: u64,
+}
+
+#[get("/code-game/user/{username}")]
+async fn get_user(username: web::Path<String>, data: web::Data<AppState>) -> HttpResponse {
+    let client = data.client.lock().unwrap();
+
+    let user: User = match sqlx::query_as(
+        "
+        SELECT username, cracked, rank FROM users
+        WHERE
+            username = $1
+
+        LIMIT 1
+        "
+    )
+    .bind(username.as_str())
+    .fetch_one(&*client).await {
+        Ok(user) => user,
+        Err(_) => {
+            return HttpResponse::NotFound().json(&WebResponse {
+                message: "User not found".to_string()
+            })
+        }
+    };
+
+    HttpResponse::Ok().json(&user)
+}
+
+#[post("/code-game/user/register")]
+async fn register_user(cred: web::Json<CredentialBody>, data: web::Data<AppState>) -> impl Responder {
+    let client = data.client.lock().unwrap();
+    match sqlx::query(
+        "
+        INSERT INTO users (username, password, cracked)
+        VALUES ($1, $2, 0)
+        "
+    )
+    .bind(cred.username.clone())
+    .bind(cred.password.clone())
+    .execute(&*client).await {
+        Ok(_) => {
+            return HttpResponse::Ok().json(&WebResponse {
+                message: "Account made.".to_string()
+            })
+        },
+        Err(_) => {
+            return HttpResponse::ImATeapot().json(&WebResponse {
+                message: "I'm a teapot.".to_string()
+            })
+        }
+    }
+}
+
+#[delete("/code-game/user/{username}")]
+async fn delete_user(cred: web::Json<CredentialBody>, data: web::Data<AppState>) -> impl Responder {
+    let client = data.client.lock().unwrap();
+    match sqlx::query(
+        "
+        DELETE FROM users
+        WHERE EXISTS (
+            username = $1
+            AND
+            password = $2
+        )
+        "
+    )
+    .bind(cred.username.clone())
+    .bind(cred.password.clone())
+    .execute(&*client).await {
+        Ok(_) => {
+            return HttpResponse::Ok().json(&WebResponse {
+                message: "Account has been deleted".to_string()
+            })
+        },
+        Err(_) => {
+            return HttpResponse::Forbidden().json(&WebResponse {
+                message: "Cannot find user.".to_string()
+            })
+        }
+    }
+}
+
+// Gives a few statistics
+#[get("/code-game/statistics")]
+async fn get_statistics(data: web::Data<AppState>) -> HttpResponse {
+    let now = Instant::now();
+
+    HttpResponse::Ok().json(&Statistics {
+        code_length: *data.code_length.read().unwrap(),
+        server_uptime: now.elapsed().as_secs()
+    })
+}
+
+#[post("/code-game/code")]
+async fn try_code(req: web::Json<WebRequest>, data: web::Data<AppState>) -> HttpResponse {
+    let mut code = data.currentcode.write().unwrap();
+    let code_length = data.code_length.write().unwrap();
+    let client = reqwest::Client::new();
+
+    if req.code == *code {
+        let mut rng = rand::thread_rng();
+        *code = Alphanumeric.sample_string(&mut rng, (*code_length).into()).to_lowercase();
+
+        let username = &*req.username;
+        let password = &*req.password;
+
+        let _ = client.post(format!("{}", dotenvy::var("WEBHOOK").unwrap()))
+            .body(serde_json::to_string(&Webhook {
+                username: format!("Code Cracker"),
+                avatar_url: None,
+                content: format!("> `[+] A code has been cracked by {}!`", username)
+            }).unwrap())
+            .header("Content-Type", "application/json")
+            .send()
+            .await;
+        
+        let client  = data.client.lock().unwrap();
+        
+        let user: User = sqlx::query_as(
+            "
+            SELECT * FROM users 
+            WHERE 
+                username = $1 
+                AND
+                password = $2
+                
+            LIMIT 1;
+            "
+        )
+        .bind(username)
+        .bind(password)
+        .fetch_one(&*client).await.unwrap();
+
+        let _ = sqlx::query(
+            "
+            UPDATE users
+            SET cracked = cracked + 1
+            WHERE
+                username = $1
+                AND
+                password = $2
+            "
+        )
+        .bind(username)
+        .bind(password)
+        .execute(&*client).await.unwrap();
+
+
+
+        println!("[-] The code has been cracked by {}!", user.username);
+        println!("[+] Current Code to crack: {}", code);
+
+        return HttpResponse::Ok().json(&WebResponse { message: ("Code Cracked!").to_string()})
+    }
+
+    HttpResponse::Unauthorized().json(&WebResponse { message: ("Incorrect Code.").to_string()})
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().unwrap();
+    let client = SqlitePoolOptions::new().connect("./db/db.sqlite?mode=rwc").await.unwrap();
+
+    let _ = sqlx::query(
+        "
+        CREATE TABLE IF NOT EXISTS users (
+            username TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL,
+            cracked INTEGER NOT NULL,
+            rank INTEGER
+        )
+        "
+    )
+    .execute(&client).await;
+    
+    let mut rng = rand::thread_rng();
+    let code = Alphanumeric.sample_string(&mut rng, 4).to_lowercase();
+
+    let currentcode = web::Data::new(AppState {
+        currentcode: RwLock::new(code.to_string()),
+        code_length: RwLock::new(4),
+        client: Arc::new(Mutex::new(client))
+    });
+
+    println!("[+] Current Code to crack: {}", code);
+
+    HttpServer::new(move || {
+        App::new()
+        .service(try_code)
+        .service(get_statistics)
+        .service(register_user)
+        .service(get_user)
+        .service(delete_user)
+        .app_data(currentcode.clone())
+    })
+    .bind(("127.0.0.1", 80))?
+    .run()
+    .await
+}
+
+// https://discord.com/api/webhooks/1322439660651417640/s3KNWUJd-fyVqcyVcWkLECg2mi6BTk18RNVLTdX3qbFgO_h0QTTX0DdKxSn7wtU4PVWr
